199420a2a1db94c77338a72b1f21d83a6b9a84f7
Alan McGurney <mcgurneyalan@gmail.com>
2022-03-10 12:21:54 -0500

    started components work

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import "./App.css";
 
 // IMPORT COMPONENTS
 import Header from "./components/header";
-import Footer from "./components/footer";
 import { Route, Routes } from "react-router-dom";
 // IMPORT PAGES
 import Home from "./pages/home";
 import About from "./pages/about";
 import Posts from "./pages/posts";
+import Users from "./pages/users";
 
 function App() {
   // URL should have YOUR HEROKU URL for your backend, make sure you include the trailing slash
@@ -18,8 +18,8 @@ function App() {
       <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects URL={URL} />} />
         <Route path="/about" element={<About URL={URL} />} />
+        <Route path="/post" element={<Post URL={URL} />} />
       </Routes>
       <Footer />
     </div>
