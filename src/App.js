1b02c3b0ac6487760b395103159fac152c705ba6
Alan McGurney <mcgurneyalan@gmail.com>
2022-03-09 22:45:26 -0500

    started comps/pages.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+// IMPORT COMPONENTS
+import Header from "./components/header";
+import Footer from "./components/footer";
+import { Route, Routes } from "react-router-dom";
+// IMPORT PAGES
+import Home from "./pages/home";
+import About from "./pages/about";
+import Posts from "./pages/posts";
 
 function App() {
+  // URL should have YOUR HEROKU URL for your backend, make sure you include the trailing slash
+  const URL = "https://express-reactlab.herokuapp.com/";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects URL={URL} />} />
+        <Route path="/about" element={<About URL={URL} />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
