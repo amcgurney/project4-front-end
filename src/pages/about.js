713939444ee917b5a921216dd33b1841d958b851
Alan McGurney <mcgurneyalan@gmail.com>
2022-03-15 16:46:02 -0400

    about page is poppin

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ import React from 'react'
   return (
     <div>
 
-      <h3><em>Founded in 2022, MFZ offers a home to any musicians to come and say what's up</em></h3>
+      <h4><em>Founded in 2022 by Alan M, MFZ offers a home for any musicians to come and say what's up</em></h4>
 
       <br></br>
       <br></br>
@@ -12,29 +12,29 @@ import React from 'react'
       <div className='test-container'>
         <figure>
           <img src="https://www.sohh.com/wp-content/uploads/2021/08/travis-scott-interview-758x569.jpg" alt="travis"/>
-          <figcaption>MFV? It's definitely lit</figcaption>
+          <figcaption><em>"MFV? Oh, it's definitely lit"</em><br></br> Travis Scott </figcaption>
         </figure>
         <figure>
           <img src="https://149354818.v2.pressablecdn.com/wp-content/uploads/2020/04/das-kope-Press-pic.jpg" alt="kope"/>
-          <figcaption>Never wanted to be in a friend-zone more</figcaption>
+          <figcaption><em>"Never wanted to be in a friendzone"</em><br></br> Das Kope </figcaption>
         </figure>
         <figure>
           <img src="https://ichef.bbci.co.uk/images/ic/1200x675/p032bbvq.jpg" alt="tame"/>
-          <figcaption>I literally sit on MFZ for hours</figcaption>
+          <figcaption><em>"I literally sit on MFZ for hours"</em><br></br> Kevin Parker </figcaption>
         </figure>
         <figure>
           <img src="https://wallpaperaccess.com/full/2373391.jpg" alt="billie"/>
-          <figcaption>blah</figcaption>
+          <figcaption><em>"Went on a few dates with the guy who built the app, he ghosted me!"</em><br></br> Billie Elish </figcaption>
         </figure>
         <figure>
           <img src="https://i.guim.co.uk/img/media/2eb2e7f476e2833be5a21440c8305f2b541677c8/224_191_1139_683/master/1139.jpg?width=465&quality=45&auto=format&fit=max&dpr=2&s=26d61f6fa9fb3b30561d5dce8bfb5dfb" alt="methyl"/>
-          <figcaption>Here before it was cool</figcaption>
+          <figcaption><em>"Here before it was cool"</em><br></br> Methyl Ethyl </figcaption>
         </figure>
         <figure>
           <img src="https://static.hiphopdx.com/2020/07/kanye-west-crying-crazy-bipolar-meds-900x505.jpg" alt="ye"/>
-          <figcaption>I can't believe I didn't come up with this first, this is not OK ya'll</figcaption>
+          <figcaption><em>"Can't believe I didn't come up with this first, it's not OK ya'll"</em><br></br> Kanye West </figcaption>
         </figure>
-        
+
       </div>
 
     </div>
